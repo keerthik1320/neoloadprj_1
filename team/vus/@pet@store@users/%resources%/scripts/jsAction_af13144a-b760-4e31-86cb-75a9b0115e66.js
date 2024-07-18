824bd4f1e40f4022d3667d099d1137bea7279528
e40012755 <e40012755@INM01587.rockwellcollins.com>
2024-07-18 13:07:12 +0530

    Checking in Petstore project

---
+++ team/vus/@pet@store@users/%resources%/scripts/jsAction_af13144a-b760-4e31-86cb-75a9b0115e66.js
@@ -0,0 +1,8 @@
+﻿var file = new java.io.File("C:\\Users\\e40012755\\Desktop\\Neoload training\\sample.csv");
+var writer = new java.io.FileWriter(file,true);
+var value1 = context.variableManager.getValue("jsessionid");
+var value2 = context.variableManager.getValue("LoginCheck");
+writer.write(value1+","+value2+"\n");
+//writer.write("Hello");
+writer.close();
+logger.debug("The values have been written to file");
